01e732144794cd2f698b949ae3c9e5a639b21105
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2017-07-21 15:25:39 -0400

    Uncommented some tests. Have one failing test right now.

--- test/EdgeTrackerTester.cpp
+++ test/EdgeTrackerTester.cpp
@@ -8,14 +8,10 @@
 struct Maker{
     mock::MockObjectMaker maker;
     mock::TopoDS_Edge mockOccEdge;
-    mock::TopoDS_Face mockOccFace;
     Trackers::EdgeTracker edgeTracker;
-    Trackers::FaceTracker faceTracker;
     Maker() :
         mockOccEdge(0),
-        mockOccFace(0),
-        edgeTracker(mockOccEdge, std::string("Edge000")),
-        faceTracker(mockOccFace, std::string("Face000"))
+        edgeTracker(mockOccEdge, std::string("Edge000"))
     {} ;
     ~Maker(){};
 };
@@ -28,19 +24,21 @@ BOOST_AUTO_TEST_CASE(GetEdge)
     BOOST_CHECK(fetchedEdge.IsEqual(mockOccEdge));
 }
 
-//BOOST_AUTO_TEST_CASE(AddFace_NoEdgeError)
-//{
-    //mock::TopoDS_Face aFace = maker.makeFace();
-    //BOOST_CHECK_THROW(edgeTracker.addFace(faceTracker), std::invalid_argument);
+BOOST_AUTO_TEST_CASE(AddFace_NoEdgeError)
+{
+    mock::TopoDS_Face aFace = maker.makeFace();
+    Trackers::FaceTracker myTracker(aFace, "Face000");
+    BOOST_CHECK_THROW(edgeTracker.addFace(myTracker), std::invalid_argument);
 
-//}
+}
 
-//BOOST_AUTO_TEST_CASE(AddFace)
-//{
-    //mock::TopoDS_Face aFace = maker.makeFace();
-    //aFace.Edges[0] = mockOccEdge;
-    //edgeTracker.addFace(aFace, "Face000");
-    //BOOST_CHECK_EQUAL(edgeTracker.isValid(), false);
-//}
+BOOST_AUTO_TEST_CASE(AddFace)
+{
+    mock::TopoDS_Face aFace = maker.makeFace();
+    aFace.Edges[0] = mockOccEdge;
+    Trackers::FaceTracker myTracker(aFace, "Face000");
+    edgeTracker.addFace(myTracker);
+    BOOST_CHECK_EQUAL(edgeTracker.isValid(), false);
+}
 
 BOOST_AUTO_TEST_SUITE_END()
