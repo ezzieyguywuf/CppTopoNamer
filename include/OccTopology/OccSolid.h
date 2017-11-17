0be311f08a914f85940aee02262b66148464fa0f
Wolfgang E. Sanyer <WolfgangESanyer@gmail.com>
2017-11-17 10:54:29 -0500

    Added OccSolid. DOES NOT COMPILE YET.

---
+++ include/OccTopology/OccSolid.h
@@ -0,0 +1,35 @@
+#ifndef OccSolid_HEADER
+#define OccSolid_HEADER
+
+#include <Topology/ISolid.h>
+#include <Topology/IFace.h>
+#include <Topology/IEdge.h>
+
+#include <TopoDS_Solid.hxx>
+
+#include <vector>
+#include <memory> // for unique_ptr, std::move, etc...
+
+using std::vector;
+using std::unique_ptr;
+
+class OccSolid : public ISolid
+{
+    public:
+        OccSolid(TopoDS_Solid aSolid);
+        OccSolid(const OccSolid& aSolid);
+        OccSolid(OccSolid&& aSolid);
+        OccSolid operator=(const OccSolid& aSolid);
+        OccSolid operator=(OccSolid&& aSolid);
+        OccSolid(){};
+
+        const vector<unique_ptr<IFace>>& getFaceVector() const;
+        const vector<unique_ptr<IEdge>>& getEdgeVector() const;
+
+    private:
+        TopoDS_Solid mySolid;
+        vector<unique_ptr<IFace>> myFaces;
+        vector<unique_ptr<IEdge>> myEdges;
+};
+
+#endif //OccSolid_HEADER
---
+++ src/OccSolid.cpp
@@ -0,0 +1,17 @@
+#include <OccTopology/OccSolid.h>
+
+OccSolid(TopoDS_Solid aSolid)
+    : mySolid(aSolid)
+{
+
+}
+OccSolid(const OccSolid& aSolid){
+}
+OccSolid(OccSolid&& aSolid){
+}
+OccSolid operator=(const OccSolid& aSolid){
+}
+OccSolid operator=(OccSolid&& aSolid){
+}
+OccSolid(){}{
+}
